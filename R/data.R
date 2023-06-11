4df2b21bbaa2d52d1d2f94e2822884e8dd1dddd9
nathanhonorato <nathanhonorato@hotmail.com>
2023-06-11 14:00:48 -0300

    Adding utils.R and data.R files

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' Results of Boston Marathon
+#'
+#' Results of finishers of Boston Marathon from 2009 to 2022.
+#'
+#' @format A dataframe with:
+#' \describe{
+#'   \item{Year}{Year of event}
+#'   \item{FullName}{Full name of the athlete}
+#'   \item{AgeOnRaceDay}{Age of the athlete on race day}
+#'   \item{Gender}{Gender of the athlete}
+#'   \item{City}{City of residence of the athlete}
+#'   \item{StateName}{State of residence of the athlete}
+#'   \item{Zip}{Zipcode of residence of the athlete}
+#'   \item{CountryOfResAbbrev}{Abbreviation of country of residence of the athlete}
+#'   \item{CountryOfResName}{Country of residence of the athlete}
+#'   \item{CountryOfCtzAbbrev}{Abbreviation of country citizenship of the athlete}
+#'   \item{CountryOfCtzName}{Country citizenship of the athlete}
+#'   \item{OfficialTime}{Official finihsing time of the athlete}
+#'   \item{RankOverall}{Overal finishing rank of the athlete}
+#'   \item{RankOverGender}{Finishing rank over gender of the athlete}
+#'   \item{RankOverDivision}{Finishing rank over division of the athlete}
+#'   \item{SubGroupLabel}{Subgroup label of the athlete}
+#'   \item{SubGroup}{Subgroup description of the athlete}
+
+#' }
+#'
+#' @source <http://registration.baa.org>
+"results_boston"
---
+++ R/utils.R
@@ -0,0 +1,2 @@
+utils::globalVariables(c("AgeOnRaceDay", "CountryOfCtzName", "FullName", "Gender", "OfficialTime",
+                       "RankOverGender", "Year", "YearFct", "results_boston"))
